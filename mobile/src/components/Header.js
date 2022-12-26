54fd1a8408cdd08bb4ef756898ef44cd63fbe280
Herisson Nobre <herisson.carvalho96@gmail.com>
2022-12-26 19:14:01 -0300

    Implements a skelekton for app header

---
+++ mobile/src/components/Header.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <View style={{ marginLeft: 15 }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 22 }}>Header</Text>
+    </View>
+  );
+};
+
+export default Header;
